8a60ff8afc585d0ffd7f968febacaa8a529df8a3
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2025-09-24 10:59:55 +0530

    uoadte

--- hashnode.blog.ajitkumarpandit.nakprc.com/packages/blog-starter-kit/themes/personal/components/layout.tsx
+++ hashnode.blog.ajitkumarpandit.nakprc.com/packages/blog-starter-kit/themes/personal/components/layout.tsx
@@ -3,6 +3,7 @@ import { BottomNavigation } from './bottom-navigation';
 import { Integrations } from './integrations';
 import { Meta } from './meta';
 import { Scripts } from './scripts';
+import { TopNav } from './top-nav';
 
 type Props = {
 	children: React.ReactNode;
@@ -13,8 +14,12 @@ export const Layout = ({ children }: Props) => {
 		<>
 			<Meta />
 			<Scripts />
+			{/* Global fixed top navigation */}
+			<TopNav />
 			<div className="min-h-screen bg-white dark:bg-neutral-950 transition-colors duration-300">
-				<main className="pb-20 lg:pb-8">{children}</main>
+				{/* Add top padding to account for fixed nav height */}
+				<main className="pt-20 pb-20 lg:pb-8">{children}</main>
+				{/* Global bottom navigation (mobile) */}
 				<div className="block lg:hidden">
 					<BottomNavigation />
 				</div>
